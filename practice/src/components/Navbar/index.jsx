32d685d7b3c13b1a390210f3deb2a31ae876cfc5
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-20 04:59:58 -0700

    working practice

--- practice/src/components/Navbar/index.jsx
+++ practice/src/components/Navbar/index.jsx
@@ -1,44 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const Navbar = () => {
+const Navbar = (props) => {
+
+const [isClicked, setIsClicked] = useState(false);
+const [abc, setAbc] = useState(false);
+
+// Initial stage
+
+// useEffect(() => {
+//   alert('Alert in use effect initail stage');
+//   }, []);
+
+// Updation stage
+
+// useEffect(() => {
+// alert('Alert in use effect updation stage '+isClicked);
+// }, [isClicked]);
+
+// UnMount stage
+
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider" /></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+
+<button onClick={() => setIsClicked(!isClicked)}>Is Clicked</button>
+<button onClick={() => setAbc(!abc)}>abc</button>
+
     </div>
   )
 }
