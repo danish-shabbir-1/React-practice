b3558a10835d4f8415e3839c57ab1d7e17aa18ff
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-31 03:53:46 -0700

    practice...

---
+++ Redux/Redux Practice/redux-app/src/Components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './../../App.css'
+
+const Navbar = () => {
+  return (
+    <div className='box-container'>
+      <div className="red box"></div>
+      <div className="green box"></div>
+      <div className="blue box"></div>
+    </div>
+  )
+}
+
+export default Navbar
